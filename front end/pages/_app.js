72df3a12429ad99b1a9cc7d3d7332e1bfea1f65f
Fabricio Sergio <fabri@example.com>
2026-04-30 18:56:24 -0300

    fix: add viewport meta tag for mobile responsiveness

--- front end/pages/_app.js
+++ front end/pages/_app.js
@@ -1,5 +1,16 @@
 import '../styles/globals.css'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+        <meta name="theme-color" content="#0a0e1a" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
